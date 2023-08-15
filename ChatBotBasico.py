9def411a3bdd807359faf1c5cffe6e3a9858f736
Matheus Lopes <99135339+MatheusLopesA95@users.noreply.github.com>
2023-08-15 15:20:03 -0300

    Update ChatBotBasico.py

--- ChatBotBasico.py
+++ ChatBotBasico.py
@@ -1,11 +1,14 @@
 import langchain 
 from aiogram import Bot, Dispatcher, executor, types
 
+#Associar o bot ao código
 bot = Bot(token = 'SEU_TOKEN')
 dp = Dispatcher(bot)
 
+#Lista de resposta
 Mensagem =['Mensagem']
 
+#Carregando os documentos utilizados para consulta pelo GPT
 loader = langchain.document_loaders.DirectoryLoader('Directory/', 
                                                     glob="**/*.txt", 
                                                     loader_cls=langchain.document_loaders.TextLoader)
@@ -18,7 +21,7 @@ text_splitter = langchain.text_splitter.RecursiveCharacterTextSplitter(chunk_siz
 texts = text_splitter.split_documents(documents)
 
 
-
+#Função para comparar a pergunta aos textos e trazer as respostas
 def query_gpt(input):
     embeddings = langchain.embeddings.OpenAIEmbeddings(openai_api_key = '')
     doc_search = langchain.vectorstores.Chroma.from_documents(texts, embeddings)
@@ -28,7 +31,7 @@ def query_gpt(input):
     Mensagem[0] = chain.run(input)
 
 
-
+#Função para receber a pergunta do usuário e responde-la pelo telegram
 @dp.message_handler()
 async def gpt(message:types.message):
     query_gpt(message.text)
